6960ec0b3e4886f8ebe332faf7e775913e6cc97a
dcndn4 <stokes@iphouse.com>
2021-11-13 16:54:53 -0600

    Today's Progress

--- Current/time_series_analysis_4Sat.py
+++ Current/time_series_analysis_4Sat.py
@@ -146,3 +146,35 @@ print(results.summary())
 pd.DataFrame(results.forecast(steps=5)[0]).plot(title="5 Day Returns Forecast")
 
 #%%
+
+# Forecasting the Settle Price using an ARIMA Model
+
+#    Using the raw Yen Settle Price, estimate an ARIMA model.
+#        Set P=5, D=1, and Q=1 in the model (e.g., ARIMA(df, order=(5,1,1))
+#        P= # of Auto-Regressive Lags, D= # of Differences (this is usually =1), Q= # of Moving Average Lags
+#    Output the ARIMA summary table and take note of the p-values of the lags. Based on the p-values, is the model a good fit (p < 0.05)?
+#    Construct a 5 day forecast for the Settle Price. What does the model forecast will happen to the Japanese Yen in the near term?
+
+
+#%%
+
+from statsmodels.tsa.arima_model import ARIMA
+
+# Estimate and ARIMA Model:
+# Hint: ARIMA(df, order=(p, d, q))
+model = ARIMA(returns['Settle'], order=(5, 1, 1))
+
+# Fit the model
+results = model.fit()
+#%%
+
+# Output model summary results:
+print(results.summary())
+
+#%%
+
+# Plot the 5 Day Price Forecast
+pd.DataFrame(results.forecast(steps=5)[0]).plot(title="5 Day Futures Price Forecast")
+
+#%%
+
